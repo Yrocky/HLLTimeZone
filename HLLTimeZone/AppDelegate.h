2790ada696dff6227d92f70987988db9e4789794
Yrocky <983272765@qq.com>
2016-02-25 16:31:55 +0800

    Initial Commit

---
+++ HLLTimeZone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLLTimeZone
+//
+//  Created by admin on 16/2/25.
+//  Copyright © 2016年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLLTimeZone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLLTimeZone
+//
+//  Created by admin on 16/2/25.
+//  Copyright © 2016年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
